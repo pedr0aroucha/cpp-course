58d2003a4e6888d841666ea3dd319b8f55dab570
pedr0aroucha <contato.pedr0aroucha@gmail.com>
2022-03-09 12:44:10 -0300

    refactoring the guessing game

--- src/jogo-da-adivinhacao.cpp
+++ src/jogo-da-adivinhacao.cpp
@@ -23,11 +23,14 @@ int main()
   cin >> guess;
   print("O valor do seu palpite é " + to_string(guess));
 
-  if (guess == secret_number)
+  bool right_guess = guess == secret_number;
+  bool guess_bigger_than_the_secret_number = guess > secret_number;
+
+  if (right_guess)
     print("Parabéns! Você acertou!");
   else
   {
-    if (guess > secret_number)
+    if (guess_bigger_than_the_secret_number)
       print("Você errou! O número secreto é menor que o seu palpite.");
     else
       print("Você errou! O número secreto é maior que o seu palpite.");
