be4459a71857595af782c56f40ad254fe88fce79
pedr0aroucha <contato.pedr0aroucha@gmail.com>
2022-03-10 10:12:24 -0300

    remove print function and add score counter

--- src/jogo-da-adivinhacao.cpp
+++ src/jogo-da-adivinhacao.cpp
@@ -2,46 +2,45 @@
 
 using namespace std;
 
-void print(string content = "")
-{
-  if (content.empty())
-    return;
-
-  cout << content;
-}
-
 int main()
 {
-  print("**************************************\n");
-  print("* Bem-vindos ao jogo da adivinhação! *\n");
-  print("**************************************\n");
+  cout << "**************************************\n";
+  cout << "* Bem-vindos ao jogo da adivinhação! *\n";
+  cout << "**************************************\n";
 
   const int SECRET_NUMBER = 19;
   int guess = 0;
   int attempts = 0;
+  double score = 1000.0;
 
   bool right_guess;
   bool guess_bigger_than_the_secret_number;
 
   while (!right_guess)
   {
-    print("Qual é o seu palpite? ");
+    cout << "Qual é o seu palpite? ";
     cin >> guess;
     attempts++;
+
+    score -= SECRET_NUMBER - guess / 2.0;
 
     right_guess = guess == SECRET_NUMBER;
     guess_bigger_than_the_secret_number = guess > SECRET_NUMBER;
 
     if (right_guess)
-      print("Parabéns! Você acertou o número secreto em " + to_string(attempts) + " tentativas!\n");
+      cout << "Parabéns! Você acertou o número secreto em " << attempts << " tentativas!\n";
     else
     {
       if (guess_bigger_than_the_secret_number)
-        print("Você errou! O número secreto é menor que o seu palpite.\n");
+        cout << "Você errou! O número secreto é menor que o seu palpite.\n";
       else
-        print("Você errou! O número secreto é maior que o seu palpite.\n");
+        cout << "Você errou! O número secreto é maior que o seu palpite.\n";
     }
   }
+
+  cout.precision(2);
+  cout << fixed;
+  cout << "Seu score foi: " << score << "\n";
 
   return 0;
 }
