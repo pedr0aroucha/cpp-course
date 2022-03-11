d79907c1a7d52ea99f882125dcd9c5b7fd08ca13
pedr0aroucha <contato.pedr0aroucha@gmail.com>
2022-03-11 10:01:16 -0300

    center game title

--- src/alura/guess_game.cpp
+++ src/alura/guess_game.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
   cout << "**************************************\n";
-  cout << "* Welcome to guess game! *\n";
+  cout << "*       Welcome to guess game!       *\n";
   cout << "**************************************\n";
 
   cout << "Chose your level \n";
