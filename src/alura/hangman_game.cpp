57f223077ea5944e673d9f36c7ddf14b4c2a3630
pedr0aroucha <contato.pedr0aroucha@gmail.com>
2022-03-12 16:31:28 -0300

    create word_exists function and Makefile

--- src/alura/hangman_game.cpp
+++ src/alura/hangman_game.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+bool word_exists(string word, char guess)
+{
+    for (char letter_in_the_word : word)
+        if (letter_in_the_word == guess)
+            return true;
+    return false;
+}
+
 int main()
 {
+    const string SECRET_WORD = "banana";
+    bool completed_word, game_over;
+
+    while (!completed_word || !game_over)
+    {
+        char guess;
+        cin >> guess;
+
+        if (word_exists(SECRET_WORD, guess))
+            cout << "You got it right, your guess is in the secret word" << endl;
+        else
+            cout << "You got it wrong, your kick is not in the secret word" << endl;
+    }
 
     return 0;
 }
