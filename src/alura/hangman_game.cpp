8ef0f1acc5b6bb2a6fc65bc093474441a976cf34
pedr0aroucha <contato.pedr0aroucha@gmail.com>
2022-03-15 09:35:47 -0300

    create guesses(map or dict)

--- src/alura/hangman_game.cpp
+++ src/alura/hangman_game.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <string>
+#include <map>
 
 using namespace std;
+
+const string SECRET_WORD = "BANANA";
+map<char, bool> guesses = {
+    {'B', false},
+    {'A', false},
+    {'N', false},
+    {'A', false},
+    {'N', false},
+    {'A', false},
+};
 
 bool word_exists(string word, char guess)
 {
@@ -13,13 +24,25 @@ bool word_exists(string word, char guess)
 
 int main()
 {
-    const string SECRET_WORD = "banana";
+
     bool completed_word, game_over;
 
     while (!completed_word || !game_over)
     {
+        for (char letter_in_the_word : SECRET_WORD)
+        {
+            if (guesses[letter_in_the_word])
+                cout << letter_in_the_word << " ";
+            else
+                cout << "_ ";
+        }
+
+        cout << endl;
+
         char guess;
         cin >> guess;
+
+        guesses[guess] = true;
 
         if (word_exists(SECRET_WORD, guess))
             cout << "You got it right, your guess is in the secret word" << endl;
